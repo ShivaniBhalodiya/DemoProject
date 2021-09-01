f563bd9617c26cc29d960a0cc62dfcb9d861e790
Shivani Bhalodiya <shivani.bhalodiya@bacancy.com>
2021-09-01 16:42:50 +0530

    Added Login and Registration module

---
+++ DemoApp/models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin", "superadmin"]
+    },
+    employeetype: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    phone:{
+      type:String,
+      required:true
+    },
+    totalexperience:{
+      type:Number,
+      required:true
+    }
+
+  },
+  { timestamps: true }
+);
+
+module.exports = model("users", UserSchema);
